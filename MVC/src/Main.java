396e151103e8fd94b5787ae101d0d0229cd52b09
lookup828 <Lookup@1x-nat-vl932-172-30-242-87.wireless.umass.edu>
2016-11-30 15:59:08 -0500

    We will be deleting this

--- MVC/src/Main.java
+++ MVC/src/Main.java
@@ -2,10 +2,15 @@ import model.*;
 import view.*;
 import controller.*;
 import mutant.*;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String ... args) {
-    	
+    	JFrame frame = new JFrame("Mutant Viewer");
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(400,400);
+    	frame.setVisible(true);
     }
 }
