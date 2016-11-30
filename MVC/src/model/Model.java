3930eac4a471cdb03d067e9798ac4a7110ce6505
reiffev <reiffev@gmail.com>
2016-11-30 15:43:15 -0500

    Updated Model.java

--- MVC/src/model/Model.java
+++ MVC/src/model/Model.java
@@ -3,23 +3,7 @@ package model;
 import view.View;
 
 public interface Model extends Iterable<Double>{
-	/**
-	 * Clear the model: remove all values.
-	 */
-	public void clear();
-	
-	/**
-	 * Add a single value to the model.
-	 * 
-	 * @param val The value to add.
-	 */
-    public void addValue(Double val);
-    
-    /**
-     * Register a view that should be notified about changes in the model.
-     * 
-     * @param v The view to register.
-     */
+
     public void register(View v);
     
     /**
