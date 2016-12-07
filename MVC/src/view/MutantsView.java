15c9575a53a18488a8527871f7873ea4ecc1af2f
Siddhartha Bhatia <sidbha@hotmail.com>
2016-12-07 15:30:56 -0500

    Added functionality to MutantsView

--- MVC/src/view/MutantsView.java
+++ MVC/src/view/MutantsView.java
@@ -3,6 +3,18 @@ package view;
 import java.util.Collection;
 
 public class MutantsView implements View{
+	
+	public JPanel getView(){
+		JPanel panel = new JPanel();
+		JScrollPane scrollView = new JScrollPane(panel);
+		scrollView.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollView.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		JPanel content = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		content.setPreferredSize(new Dimension(150,300))
+		content.add(scrollView);
+		content.setVisible(true);
+		return content; 
+	}
 
 	@Override
 	public void update(Collection<Double> data) {
