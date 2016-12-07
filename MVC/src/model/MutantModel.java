b49041e8cddf8f202904c28579f3da70a3defa84
lookup828 <Lookup@ssid-umass-vl914-172-30-11-143.wireless.umass.edu>
2016-12-07 16:23:12 -0500

    Model Stuff

---
+++ MVC/src/model/MutantModel.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Iterator;
+
+import view.View;
+import reader.TriangleMutantReader;
+
+public class MutantModel implements Model{
+
+	@Override
+	public Iterator<Double> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void register(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unregister(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
