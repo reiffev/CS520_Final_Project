2a3ad0bfd985e564b72234753f4a20de6a80e3f3
lookup828 <Lookup@ssid-umass-vl914-172-30-11-143.wireless.umass.edu>
2016-12-07 16:25:55 -0500

    Controller Stuff

---
+++ MVC/src/controller/MutantController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.Model;
+
+public class MutantController implements Controller{
+
+	@Override
+	public void setModel(Model m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Model getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
