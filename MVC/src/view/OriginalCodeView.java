2dbd84039cf328fa0bbca795f253f65912f75faf
lookup828 <Lookup@1x-nat-vl932-172-30-243-174.wireless.umass.edu>
2016-12-08 19:05:33 -0500

    OriginalCodeView Change

--- MVC/src/view/OriginalCodeView.java
+++ MVC/src/view/OriginalCodeView.java
@@ -11,21 +11,30 @@ import javax.swing.JPanel;
 import mutant.Mutant;
 
 public class OriginalCodeView implements View{
-
+	
+	private JPanel OGCodePanel;
+	
+	public OriginalCodeView(){
+		createView();
+	}
 	@Override
 	public void update(Collection<Mutant> data) {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	public JPanel getPanel(){
+	private void createView(){
 		JPanel ocv = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		ocv.setSize(250, 175);
 		ocv.setLocation(150, 30);
 		ocv.setBorder(BorderFactory.createLineBorder(Color.black));
 		ocv.add(new JLabel("Original Code"));
 		ocv.setVisible(true);
-		return ocv;
+		OGCodePanel = ocv;
+	}
+	
+	public JPanel getView(){
+		return OGCodePanel;
 	}
 
 }
